db3b5909cfb5c464ece454f5360c6e887b91fe97
chrinsley <jchrinsley02@gmail.com>
2026-05-26 15:08:58 +0400

    remove text

--- pages/dashboard.php
+++ pages/dashboard.php
@@ -274,7 +274,7 @@ $html = <<<'HTML'
                 </nav>
             </div>
         </nav>
-        hello world
+       
         <div class="container-fluid py-4">
             <div class="row">
                 <div class="col-xl-3 col-sm-6 mb-xl-0 mb-4">
